47526ad64e9e2ab06c27c5540c7d7f5b3e7c5778
PaulM17 <paulm17@gmail.com>
2025-07-11 19:22:41 +0100

    build: another commit

--- rollup.config.js
+++ rollup.config.js
@@ -20,48 +20,33 @@ const extensions = [".js", ".jsx", ".ts", ".tsx"];
 
 module.exports = {
   input: "./src/index.tsx",
-
-  // Specify external dependencies. These are not bundled with your library.
   external: Object.keys(umdGlobals),
 
   plugins: [
-    // 1. Locates modules in node_modules
     nodeResolve({ extensions }),
-
-    // 2. Converts CommonJS modules to ES6
     commonjs({ include: "**/node_modules/**" }),
-
-    // 3. Transpiles TypeScript to JavaScript.
-    // It will automatically look for a `tsconfig.json` file.
     typescript({
       tsconfig: "./tsconfig.json",
       sourceMap: true,
       inlineSources: true,
-      // The following options are added to fix the build error.
-      // We override the `declarationDir` from tsconfig.json to align with Rollup's output directory.
       declaration: true,
-      declarationDir: "dist/",
     }),
-
-    // 4. Transpiles modern JavaScript to browser-compatible versions using Babel.
-    // NOTE: Ensure your Babel config is set up for SolidJS (e.g., using 'babel-preset-solid').
     babel({
-      extensions, // Process the same files as other plugins
+      extensions,
       exclude: "**/node_modules/**",
-      babelHelpers: "bundled", // Bundles Babel helpers
+      babelHelpers: "bundled",
       presets: ["babel-preset-solid", "@babel/preset-typescript"],
     }),
 
-    // 5. Minifies the output bundle for production.
     terser(),
   ],
 
   output: {
     file: "dist/index.js",
     format: "umd",
-    name: "solidDropzone", // The global variable name for the UMD build (updated for Solid)
+    name: "solidDropzone", 
     globals: umdGlobals,
-    sourcemap: "inline", // Generates an inline sourcemap for easier debugging
+    sourcemap: "inline",
     exports: "named",
   },
 };
